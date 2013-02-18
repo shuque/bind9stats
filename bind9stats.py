e24af2ec8b29bfe8cd586a2a02842d5179eb2ada
Shumon Huque <shuque@upenn.edu>
2013-02-18 14:35:41 -0500

    A more complete initial version

--- bind9stats.py
+++ bind9stats.py
@@ -3,7 +3,14 @@
 """
 Munin plug-in for BIND9 DNS statistics server, written in Python.
 Based on the perl plug-in by George Kargiotakis <kargig[at]void[dot]gr>
-Shumon Huque <shuque - @ - upenn.edu>
+Tested with a BIND 9.9 statistics server exporting version 2.2 of
+th statistics.
+
+Author: Shumon Huque <shuque - @ - upenn.edu>
+
+Copyright (c) 2005 - 2011, Shumon Huque. All rights reserved.  
+This program is free software; you can redistribute it and/or modify 
+it under the same terms as Python itself.
 """
 
 import os, sys, time
@@ -23,6 +30,7 @@ GraphConfig = (
 
     ('dns_queries_in',
      dict(title='DNS Queries In',
+          enable=True,
           args='-l 0',
           vlabel='Queries/sec',
           location='server/queries-in/rdtype',
@@ -30,6 +38,7 @@ GraphConfig = (
 
     ('dns_server_stats',
      dict(title='DNS Server Stats',
+          enable=True,
           args='-l 0',
           vlabel='Queries/sec',
           location='server/nsstat',
@@ -42,6 +51,7 @@ GraphConfig = (
 
     ('dns_opcode_in',
      dict(title='DNS Opcodes In',
+          enable=True,
           args='-l 0',
           vlabel='Queries/sec',
           location='server/requests/opcode',
@@ -49,14 +59,16 @@ GraphConfig = (
 
     ('dns_queries_out',
      dict(title='DNS Queries Out',
+          enable=True,
           args='-l 0',
           vlabel='Count/sec',
           view='_default',
           location='rdtype',
-          config=dict(type='DERIVE', min=0))),
+          config=dict(type='DERIVE', min=0, draw='AREASTACK'))),
 
     ('dns_cachedb',
      dict(title='DNS CacheDB RRsets',
+          enable=True,
           args='-l 0',
           vlabel='Count/sec',
           view='_default',
@@ -65,6 +77,7 @@ GraphConfig = (
 
     ('dns_resolver_stats',
      dict(title='DNS Resolver Stats',
+          enable=True,
           args='-l 0',
           vlabel='Count/sec',
           view='_default',
@@ -73,8 +86,9 @@ GraphConfig = (
 
     ('dns_socket_stats',
      dict(title='DNS Socket Stats',
+          enable=True,
           args='-l 0',
-          vlabel='Count',
+          vlabel='Count/sec',
           location='server/sockstat',
           fields=("UDP4Open", "UDP6Open", 
                   "TCP4Open", "TCP6Open", 
@@ -96,55 +110,77 @@ GraphConfig = (
                   "TCP4RecvErr", "TCP6RecvErr"),
           config=dict(type='DERIVE', min=0))),
 
+    ('dns_zone_stats',
+     dict(title='DNS Zone Maintenance',
+          enable=True,
+          args='-l 0',
+          vlabel='Count/sec',
+          location='server/zonestat',
+          config=dict(type='DERIVE', min=0))),
+
 )
 
 
-def getdata(graph, getvals=False):
+def getstatsversion(etree):
+    """return version of BIND statistics"""
+    return tree.findall("%s[1]" % Path_base)[0].attrib['version']
 
+
+def getkeyvals(path, location, getvals=False):
     result = []
-    view = graph[1].get('view', None)
-
-    if view:
-        xmlpath = Path_views
-        for stat in tree.findall(xmlpath):
-            if stat.findtext('name') == view:
-                for s2 in stat.findall(graph[1]['location']):
-                    key = s2.findtext('name')
-                    if getvals:
-                        value = s2.findtext('counter')
-                        result.append((key,value))
-                    else:
-                        result.append(key)
-    else:
-        xmlpath = "%s/%s" % (Path_base, graph[1]['location'])
-        for stat in tree.findall(xmlpath):
-            key = stat.findtext('name')
-            if getvals:
-                value = stat.findtext('counter')
-                result.append((key,value))
-            else:
-                result.append(key)
-
+    for stat in path.findall(location):
+        key = stat.findtext('name')
+        if getvals:
+            value = stat.findtext('counter')
+            result.append((key,value))
+        else:
+            result.append(key)
     return result
 
 
-data = urllib2.urlopen(BINDSTATS_URL)
-tree = et.parse(data).getroot()
-data.close()
+def getdata(graph, etree, getvals=False):
+    view = graph[1].get('view', None)
+    if view:
+        xmlpath = Path_views
+        for stat in etree.findall(xmlpath):
+            if stat.findtext('name') == view:
+                return getkeyvals(stat, graph[1]['location'], getvals)
+    else:
+        xmlpath = "%s/%s" % (Path_base, graph[1]['location'])
+        return getkeyvals(etree, xmlpath, getvals)
 
-if len(sys.argv) == 2 and sys.argv[1] == "config":
+
+def validkey(graph, key):
+    fieldlist = graph[1].get('fields', None)
+    if fieldlist and (key not in fieldlist):
+        return False
+    else:
+        return True
+
+
+def get_etree_root(url):
+    """Return the root of an ElementTree structure populated by
+    parsing BIND9 statistics obtained at the given URL"""
+
+    data = urllib2.urlopen(url)
+    return et.parse(data).getroot()
+
+
+def muninconfig(etree):
+    """Generate munin config for the BIND stats plugin"""
 
     for g in GraphConfig:
+        if not g[1]['enable']:
+            continue
         print "multigraph %s" % g[0]
         print "graph_title %s" % g[1]['title']
         print "graph_args %s" % g[1]['args']
         print "graph_vlabel %s" % g[1]['vlabel']
         print "graph_category %s" % GraphCategoryName
 
-        data = getdata(g, getvals=False)
+        data = getdata(g, etree, getvals=False)
         for key in data:
-            if (not g[1].has_key('fields')) or \
-                    (g[1].has_key('fields') and (key in g[1]['fields'])):
+            if validkey(g, key):
                 print "%s.label %s" % (key, key)
                 if g[1]['config'].has_key('draw'):
                     print "%s.draw %s" % (key, g[1]['config']['draw'])
@@ -152,13 +188,28 @@ if len(sys.argv) == 2 and sys.argv[1] == "config":
                 print "%s.type %s" % (key, g[1]['config']['type'])
         print
 
-else:
+
+def munindata(etree):
+    """Generate munin data for the BIND stats plugin"""
 
     for g in GraphConfig:
+        if not g[1]['enable']:
+            continue
         print "multigraph %s" % g[0]
-        data = getdata(g, getvals=True)
+        data = getdata(g, etree, getvals=True)
         for (key, value) in data:
-            print "%s.value %s" % (key, value)
+            if validkey(g, key):
+                print "%s.value %s" % (key, value)
         print
 
-sys.exit(0)
+
+if __name__ == '__main__':
+
+    tree = get_etree_root(BINDSTATS_URL)
+
+    if len(sys.argv) == 2 and sys.argv[1] == "config":
+        muninconfig(tree)
+    else:
+        munindata(tree)
+
+    sys.exit(0)
